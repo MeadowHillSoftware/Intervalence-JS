9cbaeeaf9075486ef414e535269f564925129705
MeadowHillSoftware <meadowhillsoftware@aol.com>
2018-03-15 21:07:54 -0700

    all 2nds

--- scripts/intervalence.js
+++ scripts/intervalence.js
@@ -6,73 +6,79 @@ var oIntervalence = {};
 
 oIntervalence.addMainEventListeners = function() {
     $('#all-2nds')
-        .on('click', oIntervalence.handleButtonClick);
+        .on('click', oIntervalence.handleCheckbox);
     $('#all-3rds')
-        .on('click', oIntervalence.handleButtonClick);
+        .on('click', oIntervalence.handleCheckbox);
     $('#all-4ths')
-        .on('click', oIntervalence.handleButtonClick);
+        .on('click', oIntervalence.handleCheckbox);
     $('#all-5ths')
-        .on('click', oIntervalence.handleButtonClick);
+        .on('click', oIntervalence.handleCheckbox);
     $('#all-6ths')
-        .on('click', oIntervalence.handleButtonClick);
+        .on('click', oIntervalence.handleCheckbox);
     $('#all-7ths')
-        .on('click', oIntervalence.handleButtonClick);
+        .on('click', oIntervalence.handleCheckbox);
     $('#all-9ths')
-        .on('click', oIntervalence.handleButtonClick);
+        .on('click', oIntervalence.handleCheckbox);
     $('#all-compound')
-        .on('click', oIntervalence.handleButtonClick);
+        .on('click', oIntervalence.handleCheckbox);
     $('#all-intervals')
-        .on('click', oIntervalence.handleButtonClick);
+        .on('click', oIntervalence.handleCheckbox);
     $('#all-simple')
-        .on('click', oIntervalence.handleButtonClick);
+        .on('click', oIntervalence.handleCheckbox);
     $('#augmented-4th')
-        .on('click', oIntervalence.handleButtonClick);
+        .on('click', oIntervalence.handleCheckbox);
     $('#augmented-5th')
-        .on('click', oIntervalence.handleButtonClick);
+        .on('click', oIntervalence.handleCheckbox);
     $('#augmented-9th')
-        .on('click', oIntervalence.handleButtonClick);
+        .on('click', oIntervalence.handleCheckbox);
     $('#clear-button')
-        .on('click', oIntervalence.handleButtonClick);
+        .on('click', oIntervalence.handleClearButton);
     $('#diminished-5th')
-        .on('click', oIntervalence.handleButtonClick);
+        .on('click', oIntervalence.handleCheckbox);
     $('#diminished-7th')
-        .on('click', oIntervalence.handleButtonClick);
+        .on('click', oIntervalence.handleCheckbox);
     $('#enter-button')
-        .on('click', oIntervalence.handleButtonClick);
+        .on('click', oIntervalence.handleEnterButton);
     $('#major-2nd')
-        .on('click', oIntervalence.handleButtonClick);
+        .on('click', oIntervalence.handleCheckbox);
     $('#major-3rd')
-        .on('click', oIntervalence.handleButtonClick);
+        .on('click', oIntervalence.handleCheckbox);
     $('#major-6th')
-        .on('click', oIntervalence.handleButtonClick);
+        .on('click', oIntervalence.handleCheckbox);
     $('#major-7th')
-        .on('click', oIntervalence.handleButtonClick);
+        .on('click', oIntervalence.handleCheckbox);
     $('#major-9th')
-        .on('click', oIntervalence.handleButtonClick);
+        .on('click', oIntervalence.handleCheckbox);
     $('#major-13th')
-        .on('click', oIntervalence.handleButtonClick);
+        .on('click', oIntervalence.handleCheckbox);
     $('#minor-2nd')
-        .on('click', oIntervalence.handleButtonClick);
+        .on('click', oIntervalence.handleCheckbox);
     $('#minor-3rd')
-        .on('click', oIntervalence.handleButtonClick);
+        .on('click', oIntervalence.handleCheckbox);
     $('#minor-6th')
-        .on('click', oIntervalence.handleButtonClick);
+        .on('click', oIntervalence.handleCheckbox);
     $('#minor-7th')
-        .on('click', oIntervalence.handleButtonClick);
+        .on('click', oIntervalence.handleCheckbox);
     $('#minor-9th')
-        .on('click', oIntervalence.handleButtonClick);
+        .on('click', oIntervalence.handleCheckbox);
     $('#perfect-4th')
-        .on('click', oIntervalence.handleButtonClick);
+        .on('click', oIntervalence.handleCheckbox);
     $('#perfect-5th')
-        .on('click', oIntervalence.handleButtonClick);
+        .on('click', oIntervalence.handleCheckbox);
     $('#perfect-11th')
-        .on('click', oIntervalence.handleButtonClick);
+        .on('click', oIntervalence.handleCheckbox);
 };
 
-oIntervalence.handleButtonClick = function(event) {
+oIntervalence.handleCheckbox = function(event) {
     event.stopPropagation;
     var target = $(event.target);
     var sId = target.attr('id');
+    if (sId === "all-2nds") {
+        if (target.is(':checked')) {
+            $('#major-2nd').prop('checked', true);
+            $('#minor-2nd').prop('checked', true);
+        }
+    }
     console.log(sId);
 };
 
